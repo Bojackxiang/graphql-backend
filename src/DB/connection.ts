43c046f47227196dc97a39f58caa58d081f927a7
Alex <alex@q-83.com>
2020-10-24 21:32:44 +1100

    Code clean

---
+++ src/DB/connection.ts
@@ -0,0 +1,21 @@
+import { MongoClient } from "mongodb";
+
+class DBUtils {
+  static async connection() {
+    try {
+      const client = new MongoClient(process.env.DB_URL as string, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      await client.connect();
+
+      return client.db(process.env.DB_NAME);
+    } catch (error) {
+      console.log(" ❌ DB Connection error ");
+      console.error(error);
+      return null; 
+    }
+  }
+}
+
+export default DBUtils
--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,17 @@ require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./schema";
 // const typeDefs = require("./schema"); // 定义了 上！
-import {userResolvers} from "./resolvers";
-import { MongoClient } from "mongodb";
+import { userResolvers } from "./resolvers";
+import DBUtils from "./DB/connection";
 
 const server = new ApolloServer({
   context: async () => {
-    const client = new MongoClient(process.env.DB_URL as string, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    await client.connect();
+    const db = DBUtils.connection();
 
-    const db = client.db(process.env.DB_NAME);
-
-    return { db };
+    return { db: db ?? undefined };
   },
   typeDefs: [typeDefs],
-  resolvers: [userResolvers]
+  resolvers: [userResolvers],
 });
 
 server
